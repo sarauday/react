fafe6dc5fbbbfbae4dfa9af44a52d8e3f9ce5798
sarauday <kenit0518@gmail.com>
2023-11-19 14:40:44 +0530

    login page updated

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -23,7 +23,6 @@ export const Login = (props) => {
 
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' />
                 <button className='btn' type="submit">Login</button>
-                <label>test</label>
             </form>
             <button  className='link-btn' onClick={()=>props.onFormSwitch("login")}>Already have an account? Login Here</button>
         </div>
